951266d0c722ed9fd75ac73f1d0c4e5eebc2c785
tlfdkf301 <tlfdkf301@gmail.com>
2022-08-24 16:54:10 +0900

    initial commit

---
+++ a_minus_b.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class a_minus_b{
+    public static void main(String[] args){
+       Scanner a = new Scanner(System.in);
+       int A = a.nextInt();
+       int B = a.nextInt();
+       System.out.println(A-B);
+       a.close();
+    }
+    
+}
