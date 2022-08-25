ab7c477d3fc9f38809843cbee5f5f20273992b95
tlfdkf301 <tlfdkf301@gmail.com>
2022-08-25 15:37:37 +0900

    1008

---
+++ BaekJoon/Java/a_divide_b.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class a_divide_b{
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        int A = a.nextInt();
+        int B = a.nextInt();
+        System.out.println(A/B);
+        a.close();
+    }
+}
