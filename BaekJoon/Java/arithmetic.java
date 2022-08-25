89b7608378cb12bbb43b43dc6c210fc1fa792456
tlfdkf301 <tlfdkf301@gmail.com>
2022-08-25 15:38:09 +0900

    10869

---
+++ BaekJoon/Java/arithmetic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arithmetic {
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        int A = a.nextInt();
+        int B = a.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        a.close();
+
+    }
+    
+}
