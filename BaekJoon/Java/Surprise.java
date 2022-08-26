81715c99a64b45f4c10f785ed8f5e4aac506616a
tlfdkf301 <tlfdkf301@gmail.com>
2022-08-26 12:59:34 +0900

    10926

---
+++ BaekJoon/Java/Surprise.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Surprise {
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        String name = a.next();
+        System.out.println(name + "??!");
+        a.close();
+    }
+}
