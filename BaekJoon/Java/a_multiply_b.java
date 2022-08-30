6dd759054ab410d997567e00c78fa11451c9bb47
tlfdkf301 <tlfdkf301@gmail.com>
2022-08-30 15:37:31 +0900

    Java

--- BaekJoon/Java/a_multiply_b.java
+++ BaekJoon/Java/a_multiply_b.java
@@ -5,6 +5,7 @@ public class a_multiply_b{
        int A = a.nextInt();
        int B = a.nextInt();
        System.out.println(A*B);
+       System.out.println("good");
        a.close();
     }
     
